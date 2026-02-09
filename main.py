040b834beac672e1d75e2f172278a3858f00ef36
darlancheros <dflancheros@gmail.com>
2026-02-09 15:09:35 -0500

    nueva calculadora

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+def isiteven(num):
+    if not isitaninteger(num):
+        return False
+    return int(num) % 2 == 0
+
+def isitaninteger(num):
+    if isinstance(num, (int, float)):
+        return num == int(num)
+    return False
+
+def main():
+    print("Hello learners!")
